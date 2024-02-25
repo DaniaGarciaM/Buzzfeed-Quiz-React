002dbdcc7e89263ea3ca233effd2381c6bc02e5d
Dania Garcia <daniagm75@gmail.com>
2024-02-25 04:15:22 -0600

    Respuestas

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,8 +1,13 @@
-const QuiestionBlock = () => {
-    return(
-        <div>
-
-        </div>
+const QuiestionBlock = ({ question }) => {
+    console.log({ question })
+    return (
+        <button className="question-block">
+            <img src={question.image} alt={question.alt} />
+            <h3>{question.text}</h3>
+            <p className="id">
+                {question.id}
+            </p>
+        </button>
     );
 }
 
--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -1,9 +1,12 @@
+import QuestionBlock from './QuestionBlock';
 const QuestionsBlock = ({ quizItem }) => {
     return (
         <>
             <h2 id={quizItem.id} className="question-title">{quizItem.text}</h2>
             <div className="questions-container">
-                
+                {quizItem.questions.map(question => (
+                    <QuestionBlock key={question.id} question={question}/>
+                ))}
             </div>
         </>
     );
