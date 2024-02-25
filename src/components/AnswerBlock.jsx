a33bfc3afec6178fe65770984d35742b0d884f5d
Dania Garcia <daniagm75@gmail.com>
2024-02-25 07:48:59 -0600

    Parece quee ya va a funcionar

--- src/components/AnswerBlock.jsx
+++ src/components/AnswerBlock.jsx
@@ -3,18 +3,38 @@ import { forwardRef, useEffect, useState } from "react";
 const AnswerBlock = ({ answerOptions, chosenAnswers }, ref) => {
     const [result, setResult] = useState(null)
     console.log("dania")
+    console.log(answerOptions)
+    console.log(answerOptions[0].combination)
+    console.log(chosenAnswers)
+    // useEffect(() => {
+    //     answerOptions.forEach(answer => {
+    //         if (chosenAnswers.includes(answer.combination[0]) &&
+    //             chosenAnswers.includes(answer.combination[1]) &&
+    //             chosenAnswers.includes(answer.combination[2]) &&
+    //             chosenAnswers.includes(answer.combination[3]) &&
+    //             chosenAnswers.includes(answer.combination[4]) 
+    //         ) {
+    //             setResult(answer)
+    //         } else if (!result) {
+    //             setResult(answerOptions[0])
+    //         }
+    //     })
+    // }, [result])
     useEffect(() => {
         answerOptions.forEach(answer => {
-            if (chosenAnswers.includes(answer.combination[0]) &&
+            if (
+                answer.combination && 
+                chosenAnswers.includes(answer.combination[0]) &&
                 chosenAnswers.includes(answer.combination[1]) &&
-                chosenAnswers.includes(answer.combination[2])
+                chosenAnswers.includes(answer.combination[2]) &&
+                chosenAnswers.includes(answer.combination[3]) &&
+                chosenAnswers.includes(answer.combination[4])
             ) {
-                setResult(answer)
-            } else if (!result) {
-                setResult(answerOptions[0])
+                setResult(answer);
+                return; // Salir del bucle forEach una vez que se ha encontrado un resultado
             }
-        })
-    }, [result])
+        });
+    }, [answerOptions, chosenAnswers]);
     console.log(result)
     return (
         <div ref={ref} className="answer-block">
